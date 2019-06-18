e1abe84ac40ee08630b99d18f4e2a958495aea52
Nikolay Apanasik <apn2qq@gmail.com>
2019-06-18 22:06:00 +0300

    Worked with packages

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,38 +1,11 @@
-import { Box, Card, Image, Heading, Text, Flex } from 'rebass'
+import { Layout } from 'src/components/Layout'
+import { Hero } from 'src/components/Hero'
 
 const IndexPage = () => {
   return (
-    <Flex justifyContent="space-between">
-      <Box width={256}>
-        <Card p={1} borderRadius={2} boxShadow="0 0 16px rgba(0, 0, 0, .25)">
-          <Image src={'https://source.unsplash.com/random'} />
-          <Box px={2}>
-            <Heading as="h3">Card</Heading>
-            <Text fontSize={0}>Small meta text</Text>
-          </Box>
-        </Card>
-      </Box>
-
-      <Box width={256}>
-        <Card p={1} borderRadius={2} boxShadow="0 0 16px rgba(0, 0, 0, .25)">
-          <Image src={'https://source.unsplash.com/random'} />
-          <Box px={2}>
-            <Heading as="h3">Card</Heading>
-            <Text fontSize={0}>Small meta text</Text>
-          </Box>
-        </Card>
-      </Box>
-
-      <Box width={256}>
-        <Card p={1} borderRadius={2} boxShadow="0 0 16px rgba(0, 0, 0, .25)">
-          <Image src={'https://source.unsplash.com/random'} />
-          <Box px={2}>
-            <Heading as="h3">Card</Heading>
-            <Text fontSize={0}>Small meta text</Text>
-          </Box>
-        </Card>
-      </Box>
-    </Flex>
+    <Layout>
+      <Hero />
+    </Layout>
   )
 }
 
