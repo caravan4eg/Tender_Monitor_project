e6b7693c5a2b49df14e3a2a2bcab1371754943b5
Nikolay Apanasik <apn2qq@gmail.com>
2019-06-18 20:07:02 +0300

    Added mock data for index page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,39 @@
-export default () => <h1>Hello</h1>
+import { Box, Card, Image, Heading, Text, Flex } from 'rebass'
+
+const IndexPage = () => {
+  return (
+    <Flex justifyContent="space-between">
+      <Box width={256}>
+        <Card p={1} borderRadius={2} boxShadow="0 0 16px rgba(0, 0, 0, .25)">
+          <Image src={'https://source.unsplash.com/random'} />
+          <Box px={2}>
+            <Heading as="h3">Card</Heading>
+            <Text fontSize={0}>Small meta text</Text>
+          </Box>
+        </Card>
+      </Box>
+
+      <Box width={256}>
+        <Card p={1} borderRadius={2} boxShadow="0 0 16px rgba(0, 0, 0, .25)">
+          <Image src={'https://source.unsplash.com/random'} />
+          <Box px={2}>
+            <Heading as="h3">Card</Heading>
+            <Text fontSize={0}>Small meta text</Text>
+          </Box>
+        </Card>
+      </Box>
+
+      <Box width={256}>
+        <Card p={1} borderRadius={2} boxShadow="0 0 16px rgba(0, 0, 0, .25)">
+          <Image src={'https://source.unsplash.com/random'} />
+          <Box px={2}>
+            <Heading as="h3">Card</Heading>
+            <Text fontSize={0}>Small meta text</Text>
+          </Box>
+        </Card>
+      </Box>
+    </Flex>
+  )
+}
+
+export default IndexPage
