af688281a82adc9911615a2aa4cc8b39ce75c782
Aleksandr Apanasik <alex.apanasik@gmail.com>
2019-07-10 10:36:01 +0300

    Added home.html related url and view to test html response

--- backend/django_app/views.py
+++ backend/django_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# django_app/views.py
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
