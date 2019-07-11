93beb94ffce6e73a011baef2508d3b4d1814b822
Aleksandr Apanasik <alex.apanasik@gmail.com>
2019-07-11 08:21:22 +0300

    Added celery.shedules and run celery.beat - it's working OK!

--- backend/django_project/celery.py
+++ backend/django_project/celery.py
@@ -1,6 +1,7 @@
 # django_project/celery.py
 import os
 from celery import Celery
+from celery.schedules import crontab
  
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 app = Celery('django_project')
@@ -12,3 +13,20 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+# --------------------------------------------------------------------
+# Periodic task: run scrapy spider
+# --------------------------------------------------------------------
+
+app.conf.beat_schedule = {
+
+    'Run Scrapy QuotesSpider': {
+        'task': 'django_app.tasks.crawl',
+        'schedule': crontab(minute=0, hour='*/3'),
+                    # crontab(),  # every minute, 
+                    # crontab(minute='*/15'),  # every 15 minutes
+    },
+
+}
+
+# --------------------------------------------------------------------
